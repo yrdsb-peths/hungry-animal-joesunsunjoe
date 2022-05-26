b506935f5cf8ddd5de78c257db0f4de390ae7424
joesunsunjoe <340809144@Gapps.yrdsb.ca>
2022-05-25 21:32:27 -0400

    Added array for sounds

--- Bee.java
+++ Bee.java
@@ -6,9 +6,12 @@
  * @author Jason 
  * @version May 2022
  */
+import java.util.Random;
+
 public class Bee extends Actor
 {
     GreenfootSound beeSound = new GreenfootSound ("beeSound.mp3");
+    GreenfootSound insectSound = new GreenfootSound("insectSound.mp3");
     public void act()
     {
         if (Greenfoot.isKeyDown("left"))
@@ -17,7 +20,7 @@ public class Bee extends Actor
         }
         if (Greenfoot.isKeyDown("right"))
         {
-            move(2);            
+            move(1);            
         }
         eat();
     }
@@ -25,11 +28,32 @@ public class Bee extends Actor
     {
         if (isTouching(Baby.class))
         {
-            beeSound.play();
+            Sound();
             removeTouching(Baby.class);
             MyWorld world = (MyWorld)getWorld();
             world.createBaby();
             world.increaseScore( );
         }
+    }
+    public void Sound()
+    {
+        
+        Random rd = new Random(); 
+        int[] arr = new int[1];
+        for (int i = 0; i < arr.length; i++) 
+        {
+            arr[i] = rd.nextInt();
+            int integer = rd.nextInt();
+            if (integer%2 == 0)
+            {
+                beeSound.play();
+            }
+            else
+            {
+                insectSound.play();
+            }
+        }
+             
+            
     }
 }
